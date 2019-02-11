cc4547b76d48142461b977eec6101022531f2be4
KreizerHgz <37186178+KreizerHgz@users.noreply.github.com>
2019-02-11 11:55:04 +0100

    forgot to save...

--- src/flaskr/schema.sql
+++ src/flaskr/schema.sql
@@ -1,10 +1,26 @@
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS bruker;
 DROP TABLE IF EXISTS post;
 
-CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+CREATE TABLE bruker (
+  brukerid INTEGER PRIMARY KEY AUTOINCREMENT,
+  brukernavn TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  e-mail TEXT NOT NULL,
+  bilde IMAGE
+);
+
+CREATE TABLE jobbsøker(
+  brukerid INTEGER PRIMARY KEY REFERENCES bruker(brukerid),
+  tidligerejobber TEXT,
+  kompetanse TEXT,
+  cv TEXT,
+  fødselsdato DATE NOT NULL
+);
+
+CREATE TABLE startup(
+  brukerid INTEGER PRIMARY KEY REFERENCES bruker(brukerid),
+  beskrivelse TEXT NOT NULL,
+  oppstartsdato DATE NOT NULL
 );
 
 CREATE TABLE post (
@@ -13,5 +29,5 @@ CREATE TABLE post (
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   title TEXT NOT NULL,
   body TEXT NOT NULL,
-  FOREIGN KEY (author_id) REFERENCES user (id)
+  FOREIGN KEY (author_id) REFERENCES bruker(brukerid)
 );
