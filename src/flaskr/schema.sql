4ae713a3e99282b3d5b50f56a43045b47f421662
Sindre Johan Imenes Sivertsen <sjsivert@stud.ntnu.no>
2019-02-11 16:38:12 +0100

    #4 la til drop table

--- src/flaskr/schema.sql
+++ src/flaskr/schema.sql
@@ -1,14 +1,18 @@
 DROP TABLE IF EXISTS bruker;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS jobbsoker;
+DROP TABLE IF EXISTS startup;
+DROP TABLE IF EXISTS forsideinnlegg;
+DROP TABLE IF EXISTS tag;
 
 CREATE TABLE bruker (
   brukerid INTEGER PRIMARY KEY AUTOINCREMENT,
-  brukernavn TEXT UNIQUE NOT NULL,  passord TEXT NOT NULL,
+  brukernavn TEXT UNIQUE NOT NULL,  
+  passord TEXT NOT NULL,
   epost TEXT NOT NULL,
   bilde IMAGE
 );
 
-CREATE TABLE jobbsøker(
+CREATE TABLE jobbsoker (
   brukerid INTEGER PRIMARY KEY REFERENCES bruker(brukerid),
   tidligerejobber TEXT,
   kompetanse TEXT,
@@ -16,19 +20,20 @@ CREATE TABLE jobbsøker(
   fødselsdato DATE NOT NULL
 );
 
-CREATE TABLE startup(
+CREATE TABLE startup (
   brukerid INTEGER PRIMARY KEY REFERENCES bruker(brukerid),
   beskrivelse TEXT NOT NULL,
   oppstartsdato DATE NOT NULL
 );
 
-CREATE TABLE forsideInnlegg (
+CREATE TABLE forsideinnlegg (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   forfatter TEXT NOT NULL,
   laget DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   tittel TEXT NOT NULL,
   brødtekst TEXT NOT NULL
+);
 
-CREATE TABLE tag(
+CREATE TABLE tag (
   tagnavn TEXT PRIMARY KEY
 );
