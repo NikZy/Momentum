4cb4a405c08587fd51b9eb52f88bc0084924d0ae
Sindre Johan Imenes Sivertsen <sindrejohan@live.no>
2019-02-11 15:03:09 +0100

    #4 db tabell for forsideInnlegg

--- src/tests/data.sql
+++ src/tests/data.sql
@@ -3,6 +3,6 @@ VALUES
   (1, 'guns', '123','guns@gmail.com', 'url'),
   (2, 'Sindre','321', 'min@mail.com', 'url2');
 
-INSERT INTO post (title, body, author_id, created)
+INSERT INTO forsideInnlegg (id, forfatter, laget, tittel, brødtekst)
 VALUES
-  ('test title', 'test' || x'0a' || 'body', 1, '2018-01-01 00:00:00');
+  (1, 'Sindre', '2018-01-14', 'Første post!', 'teeeest teeext');
