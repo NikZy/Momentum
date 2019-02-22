1cf0d291688ec0f473ab5a6766743d6b0eba334d
Gunnar Hannibal <gunnar.sokjer@gmail.com>
2019-02-22 15:30:20 +0100

    la startup, tag, jobbsøknad og forsideinnlegg i db

--- src/flaskr/models.py
+++ src/flaskr/models.py
@@ -8,7 +8,7 @@ def check_password(self, password):
     return check_password_hash(self.password_hash, password)
     
 class AdminUser(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    adminid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
 
@@ -23,3 +23,30 @@ class Jobbsøker(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.emai)
+
+class Startup(db.Model):
+    startup_id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(120), nullable=False)
+    email=db.Column(db.String(50), nullable=False)
+    startup_date=db.column(db.Date)
+    description=db.column(db.String(400))
+
+    def _repr_(self):
+        return '<user{}>'.format(self.email)
+
+class Job_positions(db.Model):
+    job_positions_id=db.Column(db.Integer, primary_key=True)
+    description=db.Column(db.String(400))
+    made=db.Column(db.Date)
+    title=db.Column(db.String(32), nullable=False)
+    contact_mail=db.Column(db.String(32), nullable=False)
+
+    def _repr_(self):
+        return '<user{}>'.format(self.title)
+
+class Tag(db.Model):
+    tagname=db.Column(db.String(32), nullable=False, primary_key=True)
+
+    def _repr_(self):
+        return '<user{}>'.format(self.tagname)
+
