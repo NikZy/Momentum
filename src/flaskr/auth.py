8b516b88e26351123a86d702bfb145cff396180e
Amalie Henni <amalie.henni@gmail.com>
2019-02-25 15:23:16 +0100

    fix login logic

--- src/flaskr/auth.py
+++ src/flaskr/auth.py
@@ -64,21 +64,30 @@ def login():
         password = request.form['password']
 
         error = None
+        type = None
 
         user = db.session.query(models.AdminUser).filter(models.AdminUser.email == email).one_or_none() # ser om noen admins har det brukernavnet
-        user = db.session.query(models.Job_applicant).filter(models.Job_applicant.email == email).one_or_none() # TODO endre til Job_applicant
-        # user = db.session.query(models.Startup).filter(models.Startup.email == email).one_or_none() #TODO undo this line as comment
+        if user is not None:
+            type = 'AdminUser'
+        else:
+            user = db.session.query(models.Job_applicant).filter(models.Job_applicant.email == email).one_or_none()
+            if user is not None:
+                type = 'Job_applicant'
+            else:
+                user = db.session.query(models.Startup).filter(models.Startup.email == email).one_or_none()
+                if user is not None:
+                    type = 'Startup'
 
 
-        if (user != email): # her sto det: if (user is None):  , men skjønner ikke hvorfor så bytta det
+        if (user is None): # pga .one_or_none() i user over - da finnes ikke user i databasen
             error = 'Feil brukernavn'
         elif (not models.check_password(user, password)): # hvis feil passord
             error = 'Feil passord'
 
-        if error is None:                                           #hvis alt stemmer. fjern alt, og redirect til en side
+        if error is None: # hvis alt stemmer: fjern alt, og redirect til en side
             session.clear()
             session['user_id'] = user.id
-            session['user_type'] = 'admin'
+            session['user_type'] = type
 
             return redirect(url_for('index'))
         flash(error)
@@ -90,7 +99,11 @@ def load_logged_in_user():
     if user_id is None:
         g.user = None
     else:
-        g.user = db.session.query(models.AdminUser).filter(models.AdminUser.id == user_id).one_or_none()
+            g.user = db.session.query(models.AdminUser).filter(models.AdminUser.id == user_id).one_or_none()
+        if g.user is None:
+            g.user = db.session.query(models.Job_applicant).filter(models.Job_applicant.email == email).one_or_none()
+        if g.user is None:
+            g.user = db.session.query(models.Startup).filter(models.Startup.email == email).one_or_none()
 
 def login_required(view):           #hvis ikke logget inn, må logge inn.
     '''
