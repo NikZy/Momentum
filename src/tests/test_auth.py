563397a3c98faf0b53e70fb90ae051d6a55f951e
Sindre Johan Imenes Sivertsen <sjsivert@stud.ntnu.no>
2019-03-06 19:58:01 +0100

    la til tester for auth login

--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -1,6 +1,8 @@
-from flaskr.models import AdminUser, set_password, check_password
+from flaskr.models import AdminUser, set_password, check_password, Job_applicant
+from flask import session
 import pytest
 
+import flaskr.auth as auth 
 
 def test_add_admin_and_set_password(session):
     admin = AdminUser(username="test")
@@ -24,11 +26,32 @@ def test_register(client):
 @pytest.mark.parametrize(('first_name', 'last_name', 'email', 'password', 'type', 'date', 'message'), (
     ('', '', '','', 'Job_applicant', '2018-01-14', b'Mangler obligatoriske felter'),
     ('sindre', 'sivertsen', 'admin@admin.no','admin123', 'AdminUser', '2018-01-14', b''),
-    ('sindre', 'sivertsen', 'admin@admin.no','admin123', 'AdminUser', '2018-01-14', b'Bruker finnes'),
-    #('Sindre', 'sindre@sivertsen.no', 'passord123', 'bruker finnes fra før'),
+    ('sindre', 'sivertsen', 'admin@admin.no','admin123', 'Job_applicant', '2018-01-14', b''),
+    ('sindre', 'sivertsen', 'admin@admin.no','admin123', 'Startup', '2018-01-14', b''),
+    # ('sindre', 'sivertsen', 'admin@admin.no','admin123', 'AdminUser', '2018-01-14', b'Bruker finnes'),
 ))
 def test_register_job_applicant_validate_input(client, first_name, last_name, email, password, type, date, message):
     response = client.post('/auth/register', data={'first_name': first_name, 'last_name': last_name,
         'email': email, 'password': password,  'type': type, 'date': date, 'password': password})
 
     assert message in response.data
+
+def test_login_page(client):
+    response = client.get('/auth/login')
+    assert response.status_code == 200
+
+def test_sucsessfule_lgin(session, client, app):
+    # make user
+    user = AdminUser(email="admin@admin.no")
+    set_password(user, "admin123")
+    
+    session.add(user)
+    session.commit()
+
+    response = client.post('auth/login',data={'email':"admin@admin.no", 'password': 'admin123'})
+
+    assert not b'brukernavn' in response.data
+    response2 = client.get('/')
+    assert b'admin@admin.no' in response2.data
+# assert "/auth/login" in response.headers['Location'] 
+    # client.get('/')
