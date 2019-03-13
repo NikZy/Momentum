e36adeb9b7013b5a32bac625984bb60cd8534232
Amalie Henni <amalie.henni@gmail.com>
2019-03-13 13:14:58 +0100

    create view logic

--- src/flaskr/profile.py
+++ src/flaskr/profile.py
@@ -1,5 +1,4 @@
 import functools
-
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
@@ -8,29 +7,29 @@ import flaskr.models as models
 from flaskr import db
 
 profile_bp = Blueprint('profile', __name__, url_prefix='/profile')
-def to_datetimefield(date):
-    '''
-    Tar en string på format 2018-03-10
-    gjør om til python date object
-
-    '''
-    date = date.split('-')
-    return datetime(int(date[0]),int(date[1]), int(date[2]))
-
-@bp.route('/<int:id>/', methods=('GET', 'POST'))
+@profile_bp.route('/<int:id>/', methods=('GET', 'POST'))
 #@login_required
 def view_profile(id):
-    user_id = session.get('user_id')
-    if (not user_id):
-        # skal jo aldri skje men kunne vært greit
-        return '404'  # TODO fikse en nice 404 page
-    print("post:", post)
-    return render_template('frontpage_post/post.html', post=post)
+    error = ''
+    user = models.Startup.query.filter_by(id=id).one_or_none()
+    if user is None:
+        user = models.Job_applicant.query.filter_by(id=id).one_or_none()
+        if user is None:
+            error = '404 - fant ikke bruker' # TODO fikse en nice 404 page
+        else:
+            #type = 'job_applicant'
+            return render_template('profile/job_applicant.html', user=user)
+    else:
+        #type = 'startup'
+        return render_template('profile/startup.html', user=user)
+    #print("post:", post)
+    #return render_template('profile/post.html', post=post)
+    flash(error)
 
 
 def change_startup_info():
     return render_template('profile/startup')
 
-@bp.route('/<int:id>/', methods=('GET', 'POST'))
+#@profile_bp.route('/<int:id>/', methods=('GET', 'POST'))
 def change_job_applicant_info():
     return render_template('profile/job_applicant')
