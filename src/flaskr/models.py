8d74ba3e95fd93bd63a185ba55d57a9ef5655994
Gunnar Hannibal <gunnar.sokjer@gmail.com>
2019-03-15 16:04:50 +0100

    seed db

--- src/flaskr/models.py
+++ src/flaskr/models.py
@@ -23,7 +23,7 @@ class AdminUser(db.Model):
 
 class Job_applicant(db.Model):
     __tablename__ = 'Job_applicant'
-
+    
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     first_name = db.Column(db.String(120), nullable=False, default="")
     last_name=db.Column(db.String(120), nullable=False, default="")
@@ -32,6 +32,10 @@ class Job_applicant(db.Model):
     CV=db.Column(db.String(500))
     former_jobs=db.Column(db.String(200))
 
+    def generate_data():
+        job_applicant1=Job_applicant(first_name="Hanniballer",last_name="aldri", email="guns@gemale.com",CV="alt", former_jobs="morendin")
+        set_password(job_applicant1,mittpassord)
+        db.session.add(job_applicant1)
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
@@ -44,6 +48,12 @@ class Startup(db.Model):
     description=db.Column(db.String(300))
     password_hash = db.Column(db.String(128))
 
+    def generate_data():
+        startup1=Startup(name="smort",email="elon@tusk.nei", startup_date=2019-03-15,description="bra ide")
+        set_password(startup1, passord1)
+        db.session.add(job_applicant1)
+
+
     def _repr_(self):
         return '<user{}>'.format(self.email)
 
@@ -54,6 +64,10 @@ class Job_positions(db.Model):
     title=db.Column(db.String(32), nullable=False)
     contact_mail=db.Column(db.String(32))
 
+    def generate_data():
+        job_position1=Job_positions(description="kjip",made=2019-03-15,title=capn,contact_mail=viktig@transe)
+        db.session.add(job_position1)
+
     def _repr_(self):
         return '<user{}>'.format(self.title)
 
@@ -62,8 +76,38 @@ class Tag(db.Model):
     tagname= db.Column(db.String(32))
 
     def generate_data():
-        tag1 = Tag(tagname="test")
+        tag1 = Tag(tagname="IT")
+        tag2 = Tag(tagname="Landbruk")
+        tag3 = Tag(tagname="Økonomi og markedsføring")
+        tag4 = Tag(tagname="Bygg og anlegg")
+        tag5 = Tag(tagname="Off-shore")
+        tag6 = Tag(tagname="Miljø")
+        tag7 = Tag(tagname="Helse og sosial")
+        tag8 = Tag(tagname="Design og arkitektur")
+        tag9 = Tag(tagname="Elektro")
+        tag10 = Tag(tagname="Hotell og reise")
+        tag11 = Tag(tagname="Transport og Logistikk")
+        tag12 = Tag(tagname="Musikk og kunst")
+        tag13 = Tag(tagname="Sport og friluftsliv")
+        tag14 = Tag(tagname="Non-profit")
+        tag15 = Tag(tagname="Utdanning")
+        tag16 = Tag(tagname="Journalistikk")
         db.session.add(tag1)
+        db.session.add(tag2)
+        db.session.add(tag3)
+        db.session.add(tag4)
+        db.session.add(tag5)
+        db.session.add(tag6)
+        db.session.add(tag7)
+        db.session.add(tag8)
+        db.session.add(tag9)
+        db.session.add(tag10)
+        db.session.add(tag11)
+        db.session.add(tag12)
+        db.session.add(tag13)
+        db.session.add(tag14)
+        db.session.add(tag15)
+        db.session.add(tag16)
 
         try:
             db.session.commit()
@@ -81,7 +125,13 @@ class Frontpage_post(db.Model):
     # legge til img
     def generate_data():
         post1 = Frontpage_post(title="første post", body_text="TEEST", author=1)
+        post2 = Frontpage_post(title="heia",body_text="yass",author=1)
+        post3 = Frontpage_post(title="store nyheter!",body_text="gratis kvikk lunsj", author=1)
+        post4 = Frontpage_post(title="nede til høyre?", body_text="eller ikke",author=1)
         db.session.add(post1)
+        db.session.add(post2)
+        db.session.add(post3)
+        db.session.add(post4)
 
         try:
             db.session.commit()
@@ -95,6 +145,7 @@ import click
 from flaskr import app
 @app.cli.command()
 def seed_db ():
+    #db.drop_all()
     Tag.generate_data()
     Frontpage_post.generate_data()
 
