cc90f3fdf0968f6133c13050dfaaf1de56a03c88
Amalie Henni <amalie.henni@gmail.com>
2019-03-22 15:12:15 +0100

    merge

--- src/flaskr/profile.py
+++ src/flaskr/profile.py
@@ -14,6 +14,7 @@ def view_profile(id):
     user = models.Startup.query.filter_by(id=id).one_or_none()
     job_positions = models.Job_positions.query.all()
     if user is None:
+<<<<<<< HEAD
         user = models.Job_applicant.query.filter_by(id=id).one_or_none()
         if user is None:
             error = '404 - fant ikke bruker' # TODO fikse en nice 404 page
@@ -27,6 +28,28 @@ def view_profile(id):
     #print("post:", post)
     #return render_template('profile/post.html', post=post)
     flash(error)
+=======
+        error = '404 - fant ikke bruker' # TODO fikse en nice 404 page
+        return render_template('error_page.html')
+    else:
+        # i user ligger navn, etternavn, epost, fødselsdato, tidligere_jobber, cv
+        models.Job_applicant.query.filter_by(id=id).all() #??
+        return render_template('profile/job_applicant.html', user=user, )
+    #print("post:", post)
+    #return render_template('profile/post.html', post=post)
+    flash(error) #Trengs denne? Kan være istedet for error_page
+
+@profile_bp.route('/startup/<int:id>/', methods=('GET', 'POST'))
+def view_startup(id):
+    error = ''
+    user = models.Startup.query.filter_by(id=id).one_or_none()
+    if user is None:
+        error = '404 - fant ikke bruker' # TODO fikse en nice 404 page
+        return render_template('error_page.html')
+    else:
+        # i user ligger navn, beskrivelse, stillingsannonser, opprettelsesdato, epost.
+        return render_template('profile/startup.html', user=user)
+>>>>>>> connect html to bluprint
 
 
 def change_startup_info():
