f33107c3542ce6463ebbf693e827072cbfdc6d89
Amalie Henni <amalie.henni@gmail.com>
2019-03-22 14:30:01 +0100

    new blueprint logic

--- src/flaskr/profile.py
+++ src/flaskr/profile.py
@@ -7,31 +7,37 @@ import flaskr.models as models
 from flaskr import db
 
 profile_bp = Blueprint('profile', __name__, url_prefix='/profile')
-@profile_bp.route('/<int:id>/', methods=('GET', 'POST'))
+
 #@login_required
+@profile_bp.route('/job_applicant/<int:id>/', methods=('GET', 'POST'))
+def view_job_applicant(id):
+    error = ''
+    user = models.Job_applicant.query.filter_by(id=id).one_or_none()
+    if user is None:
+        error = '404 - fant ikke bruker' # TODO fikse en nice 404 page
+        return render_template('error_page.html')
+    else:
+            #type = 'job_applicant'
+        return render_template('profile/job_applicant.html', user=user)
+    #print("post:", post)
+    #return render_template('profile/post.html', post=post)
+    flash(error) #Trengs denne? Kan være istedet for error_page
+
+@profile_bp.route('/startup/<int:id>/', methods=('GET', 'POST'))
 def view_startup(id):
     error = ''
     user = models.Startup.query.filter_by(id=id).one_or_none()
-    job_positions = models.Job_positions.query.all()
     if user is None:
-        user = models.Job_applicant.query.filter_by(id=id).one_or_none()
-        if user is None:
-            error = '404 - fant ikke bruker' # TODO fikse en nice 404 page
-            return render_template('error_page.html')
-        else:
-            #type = 'job_applicant'
-            return render_template('profile/job_applicant.html', user=user)
+        error = '404 - fant ikke bruker' # TODO fikse en nice 404 page
+        return render_template('error_page.html')
     else:
-        #type = 'startup'
-        return render_template('profile/startup.html', user=user, job_positions=job_positions)
-    #print("post:", post)
-    #return render_template('profile/post.html', post=post)
-    flash(error)
+        return render_template('profile/startup.html', user=user)
 
+    flash(error) #Trengs denne? Kan være istedet for error_page
 
 def change_startup_info():
-    return render_template('profile/startup')
+    return render_template('profile/startup') # TODO er siste pri på liste
 
 #@profile_bp.route('/<int:id>/', methods=('GET', 'POST'))
 def change_job_applicant_info():
-    return render_template('profile/job_applicant')
+    return render_template('profile/job_applicant') # TODO er siste pri på lista
