b4be383d7962d589204ab019d03c497d32d2a8cb
KreizerHgz <37186178+KreizerHgz@users.noreply.github.com>
2019-03-26 16:02:55 +0100

    yeboi

--- src/flaskr/overview.py
+++ src/flaskr/overview.py
@@ -13,14 +13,14 @@ from flaskr import app
 
 overview_bp = Blueprint('overview', __name__, url_prefix='/overview')
 
-@overview_bp.route('/Startups/')
+@overview_bp.route('/Startups/', methods=('GET', 'POST'))
 def Startups():
     return render_template('overview/Startups.html')
 
-@overview_bp.route('/Job_applicants/')
+@overview_bp.route('/Job_applicants/', methods=('GET', 'POST'))
 def Job_applicants():
     return render_template('overview/Job_applicants.html')
 
-@overview_bp.route('/Job_positions/')
+@overview_bp.route('/Job_positions/', methods=('GET', 'POST'))
 def Job_positions():
     return render_template('overview/Job_positions.html')
